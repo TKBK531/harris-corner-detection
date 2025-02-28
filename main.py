b90756fd3e7c725d188229f203bc27572a58f607
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2025-02-28 10:42:35 +0530

    add gaussian blur and sharpening

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-filename = r"Images/Image1.jpg"
+filename = r"Images/Image6.jpg"
 
 # Read the image
 img = cv2.imread(filename)
@@ -12,12 +12,20 @@ img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = np.float32(gray)
 
-# Detect corners using Harris Corner Detection
-dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+# Apply Gaussian Blur to reduce noise
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+# Apply sharpening filter
+kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+gray = cv2.filter2D(gray, -1, kernel)
+
+# Detect corners using Harris Corner Detection with adjusted parameters
+dst = cv2.cornerHarris(gray, 3, 5, 0.06)
 dst = cv2.dilate(dst, None)
 
 # Threshold for an optimal value, it may vary depending on the image
-img[dst > 0.01 * dst.max()] = [0, 0, 255]
+threshold = 0.01 * dst.max()
+img[dst > threshold] = [0, 0, 255]
 img_rgb_corners = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 # Plot the original and corner-detected images
